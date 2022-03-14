072935862a4118555dc1b3a501668b0f9d5ede4f
cfmays <charleymays@gmail.com>
2022-03-14 14:58:20 -0400

    need to make static dir for static files;

--- ents/ents/settings.py
+++ ents/ents/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "ents" / "static"
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
